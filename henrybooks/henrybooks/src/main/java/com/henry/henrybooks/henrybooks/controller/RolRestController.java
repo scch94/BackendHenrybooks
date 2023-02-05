8e279b3cf017dd8f51d5341293f0c9bcd6b9865b
santiago canal <santycanal_06@hotmail.com>
2023-02-05 10:46:40 -0500

    crud con datos quemado

---
+++ henrybooks/henrybooks/src/main/java/com/henry/henrybooks/henrybooks/controller/RolRestController.java
@@ -0,0 +1,50 @@
+package com.henry.henrybooks.henrybooks.controller;
+
+import com.henry.henrybooks.henrybooks.entity.Rol;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rol")
+public class RolRestController {
+    @GetMapping("/{id}")
+    public Rol findById(@PathVariable long id){
+        Rol rol =new Rol(id,"user",true);
+        return rol;
+    }
+
+    @GetMapping("/")
+    public List<Rol> findAll(){
+        List<Rol> roles = new ArrayList<Rol>();
+        Rol rol1=new Rol(1,"admin",true);
+        Rol rol2=new Rol(2,"user",true);
+        roles.add(rol1);
+        roles.add(rol2);
+        return roles;
+    }
+    @PostMapping("/")
+    public Rol createRol(@RequestBody Rol rol){
+        Rol rol1 = new Rol();
+        rol1.setIdRol(3);
+        rol1.setDescripcion(rol.getDescripcion());
+        rol1.setEstado(true);
+        return rol1;
+    }
+    @PutMapping("/{id}")
+    public Rol updateRol(@PathVariable Long id,@RequestBody Rol rol){
+        Rol rol1 = findById(id);
+        rol1.setDescripcion(rol.getDescripcion());
+        rol1.setEstado(true);
+        return rol1;
+    }
+    @PutMapping("/delete/{id}")
+    public String updateRol(@PathVariable long id){
+        Rol rol1 = findById(id);
+        rol1.setEstado(false);
+        String nombre="el usuari con id "+id+" fue eliminado";
+        return nombre;
+    }
+
+}
