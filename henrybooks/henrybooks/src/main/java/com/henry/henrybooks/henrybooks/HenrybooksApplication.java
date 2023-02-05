91f304e69dd82d3bd389580480178209e320a548
santiago canal <santycanal_06@hotmail.com>
2023-02-05 01:53:12 -0500

    inicializando proyecto

---
+++ henrybooks/henrybooks/src/main/java/com/henry/henrybooks/henrybooks/HenrybooksApplication.java
@@ -0,0 +1,13 @@
+package com.henry.henrybooks.henrybooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HenrybooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HenrybooksApplication.class, args);
+	}
+
+}
